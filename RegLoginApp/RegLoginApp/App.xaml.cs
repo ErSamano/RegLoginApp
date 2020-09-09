4ec01726d2e8d8ea59ec544629f9b0ac4aca8fff
Juan Samano <jusamano@microsoft.com>
2020-09-08 23:29:47 -0500

    Creating a new starting page and add the SQLITE package on the projects

--- RegLoginApp/RegLoginApp/App.xaml.cs
+++ RegLoginApp/RegLoginApp/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using RegLoginApp.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace RegLoginApp
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new RegPage();
         }
 
         protected override void OnStart()
---
+++ RegLoginApp/RegLoginApp/Views/RegPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RegLoginApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegPage : ContentPage
+    {
+        public RegPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(Object sender, System.EventArgs e) 
+        {
+
+            //Install in all the projects SQLITE-NET-PCL
+            //Add here the connection to the database
+            throw new NotImplementedException();
+
+        }
+    }
+}
