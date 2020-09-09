c896fc3fe9e9018426918bcf3f920c1c876befa9
Juan Samano <jusamano@microsoft.com>
2020-09-09 00:01:15 -0500

    Adding database functionality

---
+++ RegLoginApp/RegLoginApp/Tables/RegUserTable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegLoginApp.Tables
+{
+    //It will hold multiple users data
+    public class RegUserTable
+    {
+
+        public Guid UserId          { get; set; }
+        public string UserName      { get; set; }
+        public string Password      { get; set; }
+        public string Email         { get; set; }
+        public string PhoneNumber   { get; set; }
+        
+        
+
+    }
+}
--- RegLoginApp/RegLoginApp/Views/RegPage.xaml.cs
+++ RegLoginApp/RegLoginApp/Views/RegPage.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using RegLoginApp.Tables;
+using SQLite;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,9 +24,25 @@ namespace RegLoginApp.Views
         {
 
             //Install in all the projects SQLITE-NET-PCL
-            //Add here the connection to the database
-            throw new NotImplementedException();
+            //Creating an internal database here for all the projects
+            var dbpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "UserDatabase.db");
+            var db = new SQLiteConnection(dbpath);
+            db.CreateTable<RegUserTable>();
 
+            var item = new RegUserTable()
+            {
+                UserName = EntryUserName.Text,
+                Password = EntryUserPassword.Text,
+                Email = EntryUserEmail.Text,
+                PhoneNumber = EntryUserPhoneNumber.Text
+            };
+
+            //Insert those values into the database
+            db.Insert(item);
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                var result = await this.DisplayAlert("Congratulations", "User Registration Sucessfull", "Yes", "Cancel");
+            });
         }
     }
 }
